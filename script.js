0f99e26b5bb17744f96326f694da9743549e1926
jurandir-Jr <jasj11@aluno.ifal.edu.br>
2026-07-16 20:29:07 -0300

    jogo-PWEB

---
+++ script.js
@@ -0,0 +1,15 @@
+let numeroSecreto = Math.floor(Math.random() * 50) + 1;
+
+let palpite = 15;
+
+while (palpite !== numeroSecreto) {
+    palpite = Number(prompt("Adivinhe o número (entre 1 e 50):"));
+
+    if (palpite < numeroSecreto) {
+        alert("O número secreto é MAIOR!");
+    } else if (palpite > numeroSecreto) {
+        alert("O número secreto é MENOR!");
+    } else {
+        alert("Parabéns! Você acertou o número secreto: " + numeroSecreto);
+    }
+}
