bdfcc8ff3f341f4dc2916ac00cbb6b6e8013d5a3
jurandir-Jr <jasj11@aluno.ifal.edu.br>
2026-07-16 21:12:21 -0300

    final

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-let numeroSecreto = Math.floor(Math.random() * 50) + 1;
 
-let palpite = 15;
+let jogador = prompt("Escolha: pedra, papel ou tesoura");
 
-while (palpite !== numeroSecreto) {   
-    palpite = Number(prompt("Adivinhe o número (entre 1 e 50):"));
+jogador = jogador.toLowerCase();
 
-    if (palpite < numeroSecreto) {
-        alert("O número secreto é MAIOR!");
-    } else if (palpite > numeroSecreto) {
-        alert("O número secreto é MENOR!");
+
+if (jogador !== "pedra" && jogador !== "papel" && jogador !== "tesoura") {
+    alert("Opção inválida! Recarregue a página e tente novamente");
+} else {
+
+    
+    let numero = Math.floor(Math.random() * 3);
+
+    // Mapeia o número para a escolha do computador
+    let computador;
+
+    if (numero === 0) {
+        computador = "pedra";
+    } else if (numero === 1) {
+        computador = "papel";
     } else {
-        alert("Parabéns! Você acertou o número secreto: " + numeroSecreto);
+        computador = "tesoura";
+    }
+
+    
+    if (jogador === computador) {
+        alert("Computador: " + computador + "\nEmpate");
+    } else if (
+        (jogador === "pedra" && computador === "tesoura") ||
+        (jogador === "papel" && computador === "pedra") ||
+        (jogador === "tesoura" && computador === "papel")
+    ) {
+        alert("Computador: " + computador + "\nVocê venceu!");
+    } else {
+        alert("Computador: " + computador + "\nVocê perdeu!");
     }
 }
